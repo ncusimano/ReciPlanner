ca933e86ed4733abf76166b5f983574ceabc11c2
ArthisRoo <arthisroo@gmail.com>
2019-11-09 15:21:48 -0700

    updated recipes class

--- RecipesClass.py
+++ RecipesClass.py
@@ -1,5 +1,32 @@
-class my_class(object):
-    pass
+class Recipes(object):
+    def __init__(name, ingredientsList, timeType, difficulty, instructions):
+    	self.name = name						# string
+    	self.ingredientsList = ingredientsList	# list of 2-cell lists
+    	self.timeType = timeType				# string
+    	self.difficulty = difficulty			# integer
+    	self.instructions = instructions		# string
+
+	def getName(self):
+		return self.name
+
+	def getIngredientsList(self):
+		return self.ingredientsList
+
+	def getTimeType(self):
+		return self.timeType
+
+	def getDifficulty(self):
+		return self.difficulty
+
+	def getInstructions(self):
+		return self.instructions
+
+	
+
+
+
+
+
 
 
 

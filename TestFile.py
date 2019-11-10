815a9098f236ab75aa86137ea7b20dc835ed4e33
Nico Millana <arthisroo@gmail.com>
2019-11-09 20:38:23 -0700

    Delete TestFile.py

--- TestFile.py
+++
@@ -1,7 +0,0 @@
-from RecipePlannerClass import RecipePlanner
-import RecipesClass
-
-if __name__ == '__main__':
-	planner = RecipePlanner()
-	planner.makeRecipe("Fun stuff", "Stuff", "Lunch", 3, "Turn up with respect")
-	print("Done!")

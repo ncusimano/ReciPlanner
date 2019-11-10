2e64fcbc2aa969749d976fa5579b5c9aa3a81b0f
ncusimano <cusimano@ualberta.ca>
2019-11-09 17:08:57 -0700

    File w/ read capabilities

--- RecipePlanner.py
+++ RecipePlanner.py
@@ -1,6 +1,8 @@
-import RecipesClass
+import os
 import pickle
+import RecipesClass
 import ingredient_list_lol
+
 
 class RecipePlanner(object):
 
@@ -12,15 +14,38 @@ class RecipePlanner(object):
         return self.__recipe_list
 
     def getRecipes():
-        pass
+        path = os.getcwd() + "/Recipes"
 
+        file_list = os.listdir(path = path)
+
+        for file in file_list:
+            recipe = ()
+            with open(path + "/" + file, 'r') as fin:
+                for i in range(1, 5):
+                    recipe.append(fin.readline())
+
+            recipe = RecipeClass(recipe[0], recipe[1], recipe[2], recipe[3], recipe[4])
+            self.__recipe_list.append(recipe)
+                    
 
     def makeRecipe(self, name, ingredientsList, timeType, difficulty, instructions):
-
         recipe = RecipesClass(name, ingredientsList, timeType, difficulty, instructions)
-        recipe.saveRecipe()
-
+        self.saveRecipe(recipe)
         return
+
+    def editRecipe():
+        pass
+
+    # Saves edited or new recipies to their own file in a recipe directory. 
+    def saveRecipe(self, recipe):
+        path = os.getcwd() +"/Recipes" + "/" + self.name + ".txt"
+
+        with open(path, 'w') as f:
+            f.write(recipe.name + "\n")
+            f.write(recipe.ingredientsList + "\n")
+            f.write(recipe.timeType + "\n")
+            f.write(recipe.difficulty + "\n")
+            f.write(recipe.instructions)
 
 
 
